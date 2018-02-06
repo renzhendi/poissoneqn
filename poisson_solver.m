6b28a6ffa816d85232ae51aa6624c132d2405c6a
Der KoD <y.m._wu@hotmail.com>
2018-02-06 10:20:08 +0000

    Timer added, New plots auto-saved
    cell2mat extremely slow, thus expensive to reproduce A every time; need
    to break down the script and pre-compute and then load A.

--- poisson_solver.m
+++ poisson_solver.m
@@ -1,14 +1,14 @@
 clear
 clc
-% This script solves general Poisson's equations numerically. It allows
-% users to input f, the RHS of Poisson's equation, and solves Au=f using
-% the simple finite difference scheme.
+% This script solves general Poisson's equations numerically. It allows users
+% to specify M and N (size of the grid), and input f (the RHS of Poisson's
+% equation), Then Au=f is solved using the simple finite difference scheme.
 
 %%%%%%%%%%%%%%%%%%%%%%
 % LHS initialization %
 %%%%%%%%%%%%%%%%%%%%%%
-N = 1000;                                                 % x, number of intervals in discretization
-M = 500;                                                 % y, number of intervals
+N = 200;                                                 % x, number of intervals in discretization
+M = 100;                                                 % y, number of intervals
 h1 = 1/N;                                                % x, interval size
 h2 = 1/M;                                                % y, interval size
 diag0 = ones(N-1,1)*2*(h1^2+h2^2)/(h1^2*h2^2);           % trivial step in constructing B
@@ -33,10 +33,14 @@ f2vec = f2mat(:);                        % (N-1)(M-1) vector
 %%%%%%%%%%%%%%%%%%
 % numerical soln %
 %%%%%%%%%%%%%%%%%%
+tic;
 u1 = A\f1vec;                % backslash solver (Gaussian)
 u1mat = vec2mat(u1,N-1);     % (M-1)*(N-1) matrix
+t1 = toc;
+tic;
 u2 = A\f2vec;
 u2mat = vec2mat(u2,N-1);
+t2 = toc;
 
 %%%%%%%%%%%%%%
 % exact soln %
@@ -52,21 +56,30 @@ s2mat = s2(x,y);
 figure(1)
 subplot(3,1,1)    % plot the numerical solution
 s11 = surf(x,y,u1mat,'FaceAlpha',0.7); s11.EdgeColor = 'none';
-axis([0 1 0 1 -1 1]); title('Numerical Solution'); colorbar;
+title(sprintf('Eqn1: M=%d, N=%d completed in %0.4f secs',M,N,t1)); 
+axis([0 1 0 1 -1 1]); colorbar;
+xlabel('x'); ylabel('y'); zlabel('numerical soln');
 subplot(3,1,2)    % plot the exact solution
 s12 = surf(x,y,s1mat,'FaceAlpha',0.7); s12.EdgeColor = 'none';
-axis([0 1 0 1 -1 1]); title('Exact Solution'); colorbar
+axis([0 1 0 1 -1 1]); colorbar;
+xlabel('x'); ylabel('y'); zlabel('exact soln');
 subplot(3,1,3)    % plot the residual (numerical - exact)
 s13 = surf(x,y,u1mat-s1mat,'FaceAlpha',0.7); s13.EdgeColor = 'none';
-title('Numerical - Exact'); colorbar;
+colorbar; xlabel('x'); ylabel('y'); zlabel('numerical - exact');
+print(sprintf('Eqn1_bs_m%d_n%d.png', M, N),'-dpng');
 
 figure(2)
 subplot(3,1,1)
 s21 = surf(x,y,u2mat,'FaceAlpha',0.7); s21.EdgeColor = 'none';
-axis([0 1 0 1 -0.001 0.004]); title('Numerical Solution'); colorbar;
+title(sprintf('Eqn2: M=%d, N=%d completed in %0.4f secs',M,N,t2)); 
+axis([0 1 0 1 -0.001 0.004]); colorbar;
+xlabel('x'); ylabel('y'); zlabel('numerical soln');
+xlabel('x'); ylabel('y'); zlabel('numerical soln');
 subplot(3,1,2)
 s22 = surf(x,y,s2mat,'FaceAlpha',0.7); s22.EdgeColor = 'none';
-axis([0 1 0 1 -0.001 0.004]); title('Exact Solution'); colorbar
+axis([0 1 0 1 -0.001 0.004]); colorbar;
+xlabel('x'); ylabel('y'); zlabel('exact soln');
 subplot(3,1,3)
 s23 = surf(x,y,u2mat-s2mat,'FaceAlpha',0.7); s23.EdgeColor = 'none';
-title('Numerical - Exact'); colorbar;
+colorbar; xlabel('x'); ylabel('y'); zlabel('numerical - exact');
+print(sprintf('Eqn2_bs_m%d_n%d.png', M, N),'-dpng');
